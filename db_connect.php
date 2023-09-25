8f4163ded9019ff3762d67b530f28a5d20500fc7
Neha Khan <111290999+Ahenkhan@users.noreply.github.com>
2023-09-26 02:09:48 +0500

    Uploaded all files whith database

---
+++ db_connect.php
@@ -0,0 +1,18 @@
+<?php
+// $servername  = "localhost";
+// $username = "root";
+// $pass = "";
+// $database = "medilab";
+
+
+// $connection = mysqli_connect($servername,$username,$pass,$database);
+
+
+$connection = mysqli_connect("localhost","root","","medilab");
+
+
+
+
+
+
+?>
